a7a9fd4ece63ae5ecabc56b9e163973057a0b4b9
yoavuzan <yoav547@gmail.com>
2025-08-20 14:22:31 +0300

    add sighn in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import SignIn from './pages/SignIn';
 
 function App() {
  
 
   return (
     <>
-    
+    <SignIn/>
     </>
   )
 }
